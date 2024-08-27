bd495b0e3695b09af24e30e78c75671ff0e919f1
n0step_ <krushnavyas06@gmail.com>
2024-08-27 18:00:17 +0000

    fix: unlock command

--- commands/unlock.js
+++ commands/unlock.js
@@ -1,16 +1,27 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { PermissionsBitField } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+const dataPath = path.join(__dirname, '../data.json');
+let channelData = require(dataPath);
+
+const saveData = () => fs.writeFileSync(dataPath, JSON.stringify(channelData, null, 2));
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('unlock')
-        .setDescription('Unlocks the voice channel'),
-    async execute(interaction) {
-        const channel = interaction.member.voice.channel;
-        if (channel) {
-            await channel.permissionOverwrites.edit(interaction.guild.roles.everyone, { Connect: true });
-            await interaction.reply({ content: 'Channel unlocked!', ephemeral: true });
-        } else {
-            await interaction.reply({ content: 'You are not in a voice channel!', ephemeral: true });
-        }
+    data: {
+        name: 'unlock',
     },
+    async execute(interaction) {
+        const channel = interaction.channel;
+        const channelInfo = channelData.channels[channel.id];
+
+        if (!channelInfo || channelInfo.ownerId !== interaction.user.id) {
+            return interaction.reply({ content: 'You are not the owner of this channel.', ephemeral: true });
+        }
+
+        await channel.permissionOverwrites.edit(interaction.guild.roles.everyone, {
+            Connect: true,
+        });
+
+        await interaction.reply({ content: 'Channel unlocked.', ephemeral: true });
+    }
 };
