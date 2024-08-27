f10595290bedb30b079ae954330eadd2dcd6ffc9
n0step_ <krushnavyas06@gmail.com>
2024-08-27 19:20:47 +0000

    fix: clicking automatic will reset vc region

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -222,16 +222,21 @@ module.exports = {
                     await interaction.reply({ content: 'You need to be in a voice channel to change its region.', ephemeral: true });
                     return;
                 }
-
+        
                 const region = interaction.values[0];
                 try {
-                    await channel.setRTCRegion(region);
-                    await interaction.reply({ content: `Voice channel region changed to ${region}.`, ephemeral: true });
+                    if (region === 'automatic') {
+                        await channel.edit({ rtcRegion: null });
+                        await interaction.reply({ content: 'Voice channel region set to automatic.', ephemeral: true });
+                    } else {
+                        await channel.setRTCRegion(region);
+                        await interaction.reply({ content: `Voice channel region changed to ${region}.`, ephemeral: true });
+                    }
                 } catch (error) {
                     console.error(error);
                     await interaction.reply({ content: 'There was an error while changing the region.', ephemeral: true });
                 }
             }
-        }
+        }        
     }
 };
