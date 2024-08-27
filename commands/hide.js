00b84f47653cef45a5f0f48603ac1236dc15e244
n0step_ <krushnavyas06@gmail.com>
2024-08-27 18:03:20 +0000

    update: added hide & unhide commands

---
+++ commands/hide.js
@@ -0,0 +1,27 @@
+const { PermissionsBitField } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+const dataPath = path.join(__dirname, '../data.json');
+let channelData = require(dataPath);
+
+const saveData = () => fs.writeFileSync(dataPath, JSON.stringify(channelData, null, 2));
+
+module.exports = {
+    data: {
+        name: 'hide',
+    },
+    async execute(interaction) {
+        const channel = interaction.channel;
+        const channelInfo = channelData.channels[channel.id];
+
+        if (!channelInfo || channelInfo.ownerId !== interaction.user.id) {
+            return interaction.reply({ content: 'You are not the owner of this channel.', ephemeral: true });
+        }
+
+        await channel.permissionOverwrites.edit(interaction.guild.roles.everyone, {
+            ViewChannel: false,
+        });
+
+        await interaction.reply({ content: 'Channel hidden.', ephemeral: true });
+    }
+};
---
+++ commands/unhide.js
@@ -0,0 +1,27 @@
+const { PermissionsBitField } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+const dataPath = path.join(__dirname, '../data.json');
+let channelData = require(dataPath);
+
+const saveData = () => fs.writeFileSync(dataPath, JSON.stringify(channelData, null, 2));
+
+module.exports = {
+    data: {
+        name: 'unhide',
+    },
+    async execute(interaction) {
+        const channel = interaction.channel;
+        const channelInfo = channelData.channels[channel.id];
+
+        if (!channelInfo || channelInfo.ownerId !== interaction.user.id) {
+            return interaction.reply({ content: 'You are not the owner of this channel.', ephemeral: true });
+        }
+
+        await channel.permissionOverwrites.edit(interaction.guild.roles.everyone, {
+            ViewChannel: true,
+        });
+
+        await interaction.reply({ content: 'Channel visible.', ephemeral: true });
+    }
+};
