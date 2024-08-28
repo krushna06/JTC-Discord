9fc2d0bd49fe08dede9488f5ade798b002a71e38
n0step_ <krushnavyas06@gmail.com>
2024-08-28 12:08:24 +0000

    update: added command descriptions to the /commands response

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const port = 3000;
 
 const commandsPath = path.join(__dirname, 'commands');
 
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 app.get('/api/v1/info', (req, res) => {
   fs.readdir(commandsPath, (err, files) => {
     if (err) {
@@ -24,8 +28,16 @@ app.get('/api/v1/info/commands', (req, res) => {
       return res.status(500).json({ error: 'Unable to read commands directory' });
     }
 
-    const commandFiles = files.filter(file => file.endsWith('.js'));
-    res.json({ commands: commandFiles });
+    const commands = files.filter(file => file.endsWith('.js')).map(file => {
+      const command = require(path.join(commandsPath, file));
+
+      return {
+        name: capitalizeFirstLetter(command.data.name),
+        description: command.data.description,
+      };
+    });
+
+    res.json({ commands });
   });
 });
 
