9bb738fe2a4b5b16389d25be531105e3a80f8a81
n0step_ <krushnavyas06@gmail.com>
2024-08-28 03:50:46 +0000

    update: new emojis to make interface better

--- commands/menu.js
+++ commands/menu.js
@@ -16,25 +16,26 @@ module.exports = {
         if (!targetChannel || targetChannel.type !== 0) {
             return interaction.reply({ content: 'You need to specify a valid text channel.', ephemeral: true });
         }
+
         const row1 = new ActionRowBuilder().addComponents(
-            new ButtonBuilder().setCustomId('lock').setEmoji('🔒').setStyle(ButtonStyle.Secondary),
-            new ButtonBuilder().setCustomId('unlock').setEmoji('🔓').setStyle(ButtonStyle.Secondary),
-            new ButtonBuilder().setCustomId('hide').setEmoji('🙈').setStyle(ButtonStyle.Secondary),
-            new ButtonBuilder().setCustomId('unhide').setEmoji('🙉').setStyle(ButtonStyle.Secondary)
+            new ButtonBuilder().setCustomId('lock').setEmoji('1278197399508226174').setStyle(ButtonStyle.Secondary),
+            new ButtonBuilder().setCustomId('unlock').setEmoji('1278197548133257226').setStyle(ButtonStyle.Secondary),
+            new ButtonBuilder().setCustomId('hide').setEmoji('1278197617217765498').setStyle(ButtonStyle.Secondary),
+            new ButtonBuilder().setCustomId('unhide').setEmoji('1278197680509681717').setStyle(ButtonStyle.Secondary)
         );
 
         const row2 = new ActionRowBuilder().addComponents(
-            new ButtonBuilder().setCustomId('setLimit').setEmoji('🔢').setStyle(ButtonStyle.Secondary),
-            new ButtonBuilder().setCustomId('invite').setEmoji('📩').setStyle(ButtonStyle.Secondary),
-            new ButtonBuilder().setCustomId('ban').setEmoji('🚫').setStyle(ButtonStyle.Secondary),
-            new ButtonBuilder().setCustomId('permit').setEmoji('✅').setStyle(ButtonStyle.Secondary)
+            new ButtonBuilder().setCustomId('setLimit').setEmoji('1278197732124786719').setStyle(ButtonStyle.Secondary),
+            new ButtonBuilder().setCustomId('invite').setEmoji('1278197927839268864').setStyle(ButtonStyle.Secondary),
+            new ButtonBuilder().setCustomId('ban').setEmoji('1278197883170062421').setStyle(ButtonStyle.Secondary),
+            new ButtonBuilder().setCustomId('permit').setEmoji('1278198032948793418').setStyle(ButtonStyle.Secondary)
         );
 
         const row3 = new ActionRowBuilder().addComponents(
-            new ButtonBuilder().setCustomId('rename').setEmoji('✏️').setStyle(ButtonStyle.Secondary),
-            new ButtonBuilder().setCustomId('claim').setEmoji('🏷️').setStyle(ButtonStyle.Secondary),
-            new ButtonBuilder().setCustomId('transfer').setEmoji('🔄').setStyle(ButtonStyle.Secondary),
-            new ButtonBuilder().setCustomId('region').setEmoji('🌍').setStyle(ButtonStyle.Secondary)
+            new ButtonBuilder().setCustomId('rename').setEmoji('1278198084274491433').setStyle(ButtonStyle.Secondary),
+            new ButtonBuilder().setCustomId('claim').setEmoji('1278198179799633941').setStyle(ButtonStyle.Secondary),
+            new ButtonBuilder().setCustomId('transfer').setEmoji('1030227495892557826').setStyle(ButtonStyle.Secondary),
+            new ButtonBuilder().setCustomId('region').setEmoji('1278198135729950762').setStyle(ButtonStyle.Secondary)
         );
 
         const embed = new EmbedBuilder()
